ccb1a733ae2776f74a319c5bab6fabdfcfa8f6ad
pom421 <mauguet.po@gmail.com>
2020-08-11 15:48:30 +0200

    feat(a11y): add language for HTML markup to improve a11y

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html lang="fr">
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
