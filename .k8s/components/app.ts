3a3e9944e089149b95d70f1b8e0c412f27ddabd9
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2020-09-25 22:46:44 +0200

    fix: Expose API_URL & POSTGRES_SSL env vars.

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,7 +1,10 @@
 import env from "@kosko/env"
 import { ok } from "assert"
+import { EnvVar } from "kubernetes-models/v1/EnvVar"
+import { addEnv } from "@socialgouv/kosko-charts/utils/addEnv"
 import { create } from "@socialgouv/kosko-charts/components/app"
 import { Deployment } from "kubernetes-models/apps/v1/Deployment"
+import { getIngressHost } from "@socialgouv/kosko-charts/utils/getIngressHost"
 import { addPostgresUserSecret } from "@socialgouv/kosko-charts/utils/addPostgresUserSecret"
 
 const manifests = create("app", {
@@ -28,6 +31,13 @@ const appDeployment = manifests.find(
   (manifest): manifest is Deployment => manifest.kind === "Deployment",
 )
 ok(appDeployment)
+
+const apiUrl = new EnvVar({
+  name: "API_URL",
+  value: `https://${getIngressHost(manifests)}/api`,
+})
+
+addEnv({ deployment: appDeployment, data: apiUrl })
 
 addPostgresUserSecret(appDeployment)
 
