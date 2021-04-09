768a69c655dc7f0713f72d0a807ac0521df6c618
pom421 <mauguet.po@gmail.com>
2021-04-09 11:56:13 +0200

    fix: fix tests for steps

--- src/__tests__/models/declarations.test.js
+++ src/__tests__/models/declarations.test.js
@@ -38,7 +38,7 @@ test("A incorrect declaration", async () => {
   }
 
   await expect(validateJS(declaration)).rejects.toMatchInlineSnapshot(
-    `[Error: Données invalides  (declarations modèle)]`,
+    `[Error: Données invalides  (declarations/liberal modèle)]`,
   )
   console.error = consoleErrorLegacy
 })
