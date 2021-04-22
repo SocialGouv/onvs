97d2d5f536949311d1f76033ce210393891f0a9f
pom421 <mauguet.po@gmail.com>
2021-04-22 22:42:31 +0200

    fix: job data was missing

--- src/components/wizard/formReducer.js
+++ src/components/wizard/formReducer.js
@@ -33,6 +33,7 @@ export const formReducer = (state, payload) => {
     case "INIT": {
       return {
         ...buildEmptyState(),
+        ...buildState({ data, state, step, stepName }),
         declarationType,
       }
     }
