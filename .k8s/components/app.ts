ae7a774610bc051d7465f621dd96d75f61c056a4
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-01 14:52:28 +0200

    feat(ci): use kaniko + harbor

--- .k8s/components/app.ts
+++ .k8s/components/app.ts
@@ -1,9 +1,14 @@
 import env from "@kosko/env"
 import { create } from "@socialgouv/kosko-charts/components/app"
+import { getHarborImagePath } from "@socialgouv/kosko-charts/utils"
 
 const manifests = create("app", {
   env,
-  config: { containerPort: 3000, withPostgres: true },
+  config: {
+    containerPort: 3000,
+    withPostgres: true,
+    image: getHarborImagePath({ name: "onvs-app" }),
+  },
   deployment: {
     container: {
       resources: {
