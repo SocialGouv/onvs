644daf327da22091fab606a344f59488c121269f
Douglas DUTEIL <douglasduteil@gmail.com>
2021-06-15 11:28:01 +0200

    test: update snapshots

--- .k8s/components/pg.ts
+++ .k8s/components/pg.ts
@@ -1,13 +1,13 @@
 import env from "@kosko/env"
 import { SealedSecret } from "@kubernetes-models/sealed-secrets/bitnami.com/v1alpha1/SealedSecret"
-
+import { create } from "@socialgouv/kosko-charts/components/azure-pg"
+import gitlab from "@socialgouv/kosko-charts/environments/gitlab"
 import { loadYaml } from "@socialgouv/kosko-charts/utils/getEnvironmentComponent"
 import { updateMetadata } from "@socialgouv/kosko-charts/utils/updateMetadata"
-import gitlab from "@socialgouv/kosko-charts/environments/gitlab"
 
-import { create } from "@socialgouv/kosko-charts/components/azure-pg"
 
-export default () => {
+
+export default async () => {
   if (env.env === "dev") {
     return create({
       env,
@@ -15,7 +15,7 @@ export default () => {
   }
 
   // in prod/preprod, we try to add a fixed sealed-secret
-  const secret = loadYaml<SealedSecret>(env, `pg-user.sealed-secret.yaml`)
+  const secret = await loadYaml<SealedSecret>(env, `pg-user.sealed-secret.yaml`)
   if (!secret) {
     return []
   }
