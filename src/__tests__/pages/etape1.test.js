aa33f6dacab46c761af8cde6ba22e712d13b56de
pom421 <mauguet.po@gmail.com>
2021-07-01 14:54:37 +0200

    chore: update libs with minor versions

--- src/__tests__/pages/etape1.test.js
+++ src/__tests__/pages/etape1.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { formatISO } from "date-fns"
