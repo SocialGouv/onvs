1ceab48c3e6ca698d1bb2556f4e6f003f2b181db
pom421 <mauguet.po@gmail.com>
2021-07-16 12:15:52 +0200

    feat: add notNullable columns on declarations table

--- src/pages/api/stats.js
+++ src/pages/api/stats.js
@@ -16,17 +16,13 @@ const handler = async (req, res) => {
       case "GET": {
         const [date] = await knex.select(knex.raw("now()"))
 
-        const [globalCount] = await knex("declarations")
-          .whereNull("deleted_at")
-          .count()
+        const [globalCount] = await knex("declarations").count()
 
         const [freelanceCount] = await knex("declarations")
-          .whereNull("deleted_at")
           .where("declaration_type", "liberal")
           .count()
 
         const [freelanceCount31Days] = await knex("declarations")
-          .whereNull("deleted_at")
           .where("declaration_type", "liberal")
           .whereRaw("created_at > now() - interval '31 days'")
           .count()
