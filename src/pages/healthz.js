50a4781e1aebcbdb0aa4b52e531b222728472882
pom421 <mauguet.po@gmail.com>
2021-09-23 14:55:40 +0200

    feat(probe): test of correct Prisma/DB behavior

--- src/pages/healthz.js
+++ src/pages/healthz.js
@@ -1,4 +1,19 @@
 import React from "react"
+import prisma from "@/prisma/db"
 
 const Page = () => <div>It Works!</div>
 export default Page
+
+// Probe to detect correct Prisma/DB behavior
+export async function getServerSideProps() {
+  console.log("Appel de la page healtz :")
+  try {
+    await prisma.declaration.count()
+
+    console.log(` -> page healtz OK`)
+    return { props: {} }
+  } catch (error) {
+    console.error(` -> page healtz KO`)
+    throw error
+  }
+}
